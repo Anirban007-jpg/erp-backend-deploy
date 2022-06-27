0afb27e9444782c0701c6add5c323dc77ca39e5e
Anirban Banerjee <abanerjee763@outlook.com>
2022-06-27 09:59:26 +0530

    Closes #1

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const { Register} = require('../controllers/auth');
+const { Register,Login} = require('../controllers/auth');
 
-const {userRegisterValidator} = require('../validators/auth');
+const {userRegisterValidator, userSigninValidator} = require('../validators/auth');
 const {runValidations} = require('../validators/index');
 
 const router = express.Router();
 
 // Create the Register route using validator functions
 router.post("/register", userRegisterValidator, runValidations, Register);
-
+router.post("/login", userSigninValidator, runValidations, Login);
 
 module.exports = router;
